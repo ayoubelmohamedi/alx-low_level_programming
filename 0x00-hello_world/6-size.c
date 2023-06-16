804bbe0b4aa95efc1185740a9f214e7ee92b5712
ayoubelmohamedi <ayoubprog61@gmail.com>
2023-06-16 10:46:58 -0700

    get size of datatypes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{	
+	const char* dataTypes[] = {"char", "int", "long int", "long long int", "float"};
+	for (int i = 0 ; i < sizeof(dataTypes); i++){
+		printf("Size of %s: %zu byte(s)\n", dataTypes[i], sizeof(dataTypes[i]));
+	}
+	return (0);
+}
