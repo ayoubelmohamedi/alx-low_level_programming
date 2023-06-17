ab01dfd436e0288418df6cacabb690d57b5afd78
ayoubelmohamedi <ayoubprog61@gmail.com>
2023-06-17 05:15:28 -0700

    fix tailing whitespace

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - assigns a random number to int n everytime
@@ -11,12 +11,13 @@ int main(void)
 {
 	int n;
 
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
 	else if (n < 0)
-		printf("%d is negative\n", n);	
+		printf("%d is negative\n", n);
 	return (0);
 }
