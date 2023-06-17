6d52ca28370431724041b415390541bc56fceafa
ayoubelmohamedi <ayoubprog61@gmail.com>
2023-06-17 05:28:05 -0700

    remove implicit dec

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - prints the last digit of the random
@@ -18,6 +19,6 @@ int main(void)
 	else if (n % 10 == 0)
 		printf("and is 0\n");
 	else if (n % 10 < 6 && n % 10 != 0)
-		printf("and is less than 6 and not 0\n");	
+		printf("and is less than 6 and not 0\n");
 	return (0);
 }
