f730a73ae94b9253e8dfb1a9473d364658ea88e8
ayoubelmohamedi <ayoubprog61@gmail.com>
2023-06-20 17:44:43 -0700

    5-sign.c | the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+		return (1);
+	else if (n < 0)
+		_putchar('-');
+		return (-1)
+	_putchar('0');
+	return (0);
+		
+}
