2b487fb5f1d3026095fa25002e91b8248da6a023
ayoubelmohamedi <ayoubprog61@gmail.com>
2023-06-23 00:27:44 +0100

    change func name, rm redundancy

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
@@ -7,12 +6,10 @@
  * it executes, and prints it
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+int positive_or_negative(int n)
+{
+
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
