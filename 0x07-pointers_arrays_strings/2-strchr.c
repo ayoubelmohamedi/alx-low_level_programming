13dbb7eb201a0cb9a35a9a00328b29020b5337ee
ayoubelmohamedi <ayoubprog61@gmail.com>
2023-07-04 07:04:08 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,18 @@
  */
 char *_strchr(char *s, char c)
 {
+		int a;
 
-        while (s != '\0')
-        {
-                if (s == c)
-                	return (s);
-                *s++;
-        }
-        return (NULL);
+		while (1)
+		{
+			a = *s++;
+			if (a == c)
+			{
+				return (s - 1);
+			}
+			if (a == 0)
+			{
+				return (NULL);
+			}
+		}
 }
