66b25771e28edd8edb5cadc880a8e643694d96aa
ayoubelmohamedi <ayoubprog61@gmail.com>
2023-07-21 02:19:35 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,27 +1,33 @@
-#include "function_pointers.h"
+#include "3-calc.h"
 
 /**
- * int_index - earches for an integer
- * @array: array to search in
- * @size: size of the array
- * @cmp: pointer to the comparing function
+ * get_op_func - function pointer that selects the correct function to perform
+ * the operation asked by the user
+ * @s: the operator given by the user
  *
- * Return: index of the first element for which
- * the cmp function does not return 0, or -1 if no match is found
- * or size is negative
+ * Return: pointer to the function that corresponds to the
+ * operator given as a parameter
  */
-int int_index(int *array, int size, int (*cmp)(int))
+int (*get_op_func(char *s))(int, int)
 {
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 	int i;
 
-	if (array && cmp)
+	i = 0;
+
+	while (ops[i].op)
 	{
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]) != 0)
-				return (i);
-		}
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
 	}
 
-	return (-1);
+	return (NULL);
 }
